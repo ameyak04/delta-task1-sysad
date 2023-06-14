ab8737f2a52b224b07555e417422e18dbcd92d84
Ameya Kanagalekar <131241543+ameyak04@users.noreply.github.com>
2023-06-14 17:01:12 +0530

    Delete messwebsite.sh

--- messwebsite.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo -e "Enter your IP address : \n"
-read your_server_IP
-
-mkdir -p /var/www/html/gamma-z.hm
-chown -R $USER:$USER /var/www/html/gamma-z.hm
-chmod -R 755 /var/www
-
-echo -e "\n$your_server_IP gamma-z.hm \n" >> /etc/hosts
